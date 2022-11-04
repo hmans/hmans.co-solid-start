00292d56485b3d91dbd726f2f8fee2fdee946755
Hendrik Mans <hendrik@mans.de>
2022-11-04 21:30:09 +0100

    Clear CSS

--- src/root.tsx
+++ src/root.tsx
@@ -22,6 +22,7 @@ export default function Root() {
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
       <Body>
         <ErrorBoundary>
           <A href="/">Index</A>
