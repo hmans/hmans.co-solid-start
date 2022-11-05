dbffc28ce8b895d5a2d682adef4276898b0d8d23
Hendrik Mans <hendrik@mans.de>
2022-11-05 12:16:28 +0100

    glob

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,46 @@
 import { For, Suspense } from "solid-js";
 import { A } from "solid-start";
 
-const foo = import.meta.glob("./posts/*.{md,mdx}", {
-  eager: true,
-  query: { meta: "" },
-});
+type SolidStartFunctions<Frontmatter> = {
+  getHeadings: () => {
+    depth: number;
+    text: string;
+    slug: string;
+  }[];
+  getFrontMatter: () => Frontmatter;
+};
+
+function glob(mask: string) {
+  return import.meta.glob<
+    true,
+    any,
+    SolidStartFunctions<{
+      title?: string;
+      sectionTitle?: string;
+      order?: number;
+      section?: string;
+      sectionOrder?: number;
+      subsection?: string;
+    }>
+  >("./posts/*.{md,mdx}", {
+    eager: true,
+    query: { meta: "" },
+  });
+}
+
+const posts = glob("./posts/*.{md,mdx}");
 
 export default function Index() {
   return (
     <>
       <h1>Hendrik Mans</h1>
       <Suspense>
-        <For each={Object.entries(foo)}>
+        <For each={Object.entries(posts)}>
           {([key, value]) => {
+            const fm = value.getFrontMatter();
             return (
               <p>
-                <A href={key}>Post Title Here</A>
+                <A href={key}>{fm.title}</A>
               </p>
             );
           }}
--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,6 @@ import mdx from "solid-start-mdx";
 export default defineConfig({
   plugins: [
     await mdx(),
-    {
-      ...(await import("@mdx-js/rollup")).default({
-        jsx: true,
-        jsxImportSource: "solid-js",
-        providerImportSource: "solid-mdx",
-      }),
-      enforce: "pre",
-    },
     solid({
       adapter: "solid-start-vercel",
       extensions: [".mdx", ".md"],
