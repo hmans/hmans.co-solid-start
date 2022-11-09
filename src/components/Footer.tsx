af4539114df22432ed73a4cc5c8b3c2dcb6d45c5
Hendrik Mans <hendrik@mans.de>
2022-11-09 17:50:44 +0100

    CSS modules, woohoo!

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
+import css from "./Footer.module.css";
+
 export default function Footer() {
   return (
-    <footer role="main">
+    <footer role="main" class={css.footer}>
       Made with <span style="color: #e25555;">&hearts;</span> in Wedel by
       Hendrik Mans, 2022
     </footer>
